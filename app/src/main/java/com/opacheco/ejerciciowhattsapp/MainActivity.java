d68927d71fdf75769dd971087a9f905c0f6ec908
CFGS <interpacheco@gmail.com>
2021-02-10 20:55:48 +0100

    Initial commit

---
+++ app/src/main/java/com/opacheco/ejerciciowhattsapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.opacheco.ejerciciowhattsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_opciones, menu);
+        return true;
+    }
+
+}
+
+

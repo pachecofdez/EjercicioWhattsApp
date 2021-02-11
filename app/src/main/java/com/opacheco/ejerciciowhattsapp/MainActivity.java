2386d18afe376f368eefd7f098238a98f298936b
pachecofdez <interpacheco@gmail.com>
2021-02-11 12:15:02 +0100

    Creado el ListView

--- app/src/main/java/com/opacheco/ejerciciowhattsapp/MainActivity.java
+++ app/src/main/java/com/opacheco/ejerciciowhattsapp/MainActivity.java
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listaChats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaChats = findViewById(R.id.ListViewId);
+        ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(this, R.array.arrayChats, android.R.layout.simple_list_item_1);
+        listaChats.setAdapter(adaptador);
+
     }
 
     @Override
@@ -20,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_opciones, menu);
         return true;
     }
+
+
+
 
 }
 
